968c36a987f5c33b2dd7a3e348d05b0cec73a8bf
Aaron Prindle <prindle.aaron@gmail.com>
2013-03-08 04:08:37 -0500

    added proper deffered stuff, upgraded to 2.7

--- main.py
+++ main.py
@@ -9,29 +9,22 @@ from google.appengine.ext.webapp \
      import template    
 
 class Shout(ndb.Model):
-    message = ndb.StringProperty(
-        required=True)
-    when = ndb.DateTimeProperty(
-        auto_now_add=True)
+    message = ndb.StringProperty(required=True)
+    when = ndb.DateTimeProperty(auto_now_add=True)
     #ADDED
-    author = ndb.StringProperty(
-        required=True)
+    author = ndb.StringProperty(required=True)
 
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
         shouts = ndb.gql(
-        'SELECT * FROM Shout')
-        values = {
-            'shouts':shouts
-        }
+            """SELECT * FROM Shout
+            LIMIT 10""")
+        values = {'shouts':shouts}
         self.response.out.write(
           template.render('main.html',values))
     def post(self):
-        shout = Shout(
-        message=self.request.get(
-            'message'),author=self.request.get(
-            'author') )
+        shout = Shout(message=self.request.get('message'),author=self.request.get('author') )
         shout.put()
         self.redirect('/')
 
@@ -40,10 +33,5 @@ class UpdateHandler(webapp.RequestHandler):
         deferred.defer(update_schema.UpdateSchema)
         self.response.out.write('Schema migration successfully initiated.')
 
-def main():
-    app = webapp.WSGIApplication([
-        (r'/',MainHandler),('/update_schema', UpdateHandler)], debug=True)
-    wsgiref.handlers.CGIHandler().run(app)
-
-if __name__ == "__main__":
-    main()
+app = webapp.WSGIApplication([
+    (r'/',MainHandler),('/update_schema', UpdateHandler)], debug=True)
--- update_schema.py
+++ update_schema.py
@@ -1,13 +1,24 @@
+import time
 import main
+from google.appengine.ext import deferred
 from google.appengine.ext import ndb
 
 
+BATCH_SIZE = 1  # ideal batch size may vary based on entity size.
+
+
 #for more: https://developers.google.com/appengine/articles/update_schema
-def UpdateSchema(cursor=None, num_updated=0):
-    query = ndb.gql(
-        'SELECT * FROM Shout')
-    
-    for p in query:
-        if p.author == "Anonymous" or p.author == None:
-            p.author = "New Name"
-            p.put()
+def UpdateSchema(cur_offset=0):
+    query = main.Shout.query()
+
+    to_put=[]
+    for p in query.fetch(limit=BATCH_SIZE, offset=cur_offset):
+        p.author = "Working"
+        to_put.append(p)
+
+            
+    if to_put:
+        p.put()
+        time.sleep(60)
+        deferred.defer(
+            UpdateSchema, cur_offset= cur_offset + BATCH_SIZE)
